ca156e013dd9cd3846a2bff31cba492ed95d8bd2
Niels de Bruin <nielsdB97@gmail.com>
2018-02-12 03:35:43 +0100

    First version🎉

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var del = require('del');
+var browserSync = require('browser-sync').create();
+
+var paths = {
+  html: {
+    src: 'src/index.html',
+    dest: 'dist/',
+  },
+  styles: {
+    src: 'src/styles.css',
+    dest: 'dist/css',
+  },
+  scripts: {
+    src: 'src/scripts.js',
+    dest: 'dist/js/',
+  }
+}
+
+function clean() {
+  return del(['dist']);
+}
+
+function copyHTML() {
+  return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest));
+}
+
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+      grid: true
+    }))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
+}
+function scripts() {
+  return gulp.src(paths.scripts.src)
+    .pipe(babel({
+      presets: ['@babel/env'],
+      minified: true,
+      comments: false
+    }))
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function watch() {
+  gulp.watch(paths.html.src, copyHTML);
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.scripts.src, scripts);
+}
+
+function webserver() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    },
+    open: false
+  });
+}
+
+var build = gulp.series(clean, gulp.parallel(copyHTML, styles, scripts));
+
+gulp.task('build', build);
+gulp.task('default', gulp.series(build, gulp.parallel(webserver, watch)));
